23a8e86359fa9007ddcc9139b394069a7fbb480f
Jeremie Bidaux <jeremie.bidaux@gmail.com>
2024-09-05 15:46:00 +0200

    still not working..

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -4,7 +4,7 @@
 #create the system tables that MySQL needs to function, 
 #along with the root user and other MySQL system users,
 #if they don't already exist.
-mysql_upgrade
+mysql_install_db
 
 #This command starts the MySQL server daemon. 
 #mysqld is the main program that does most of the work 
