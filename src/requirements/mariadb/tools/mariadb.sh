9cb62829bce65b9c4e50c205749a31d866ead1c0
Jeremie Bidaux <jeremie.bidaux@gmail.com>
2024-10-02 12:18:20 +0200

    mariadb done ?

---
+++ src/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+service mysql start
+
+db_name = ${DB_NAME}
+db_user = ${DB_USER}
+db_pwd = ${DB_PWD}
+
+echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+echo "FLUSH PRIVILEGES;" >> db1.sql
+
+mysql < db1.sql
+
+mysqld
