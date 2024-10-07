d0018825ed285f37a1ff40d34f53a8a8df6e7d51
Jeremie Bidaux <jeremie.bidaux@gmail.com>
2024-10-07 07:15:03 +0200

    after ajar

--- src/requirements/mariadb/tools/mariadb.sh
+++ src/requirements/mariadb/tools/mariadb.sh
@@ -1,17 +1,70 @@
-#!/bin/bash
+# #!/bin/sh
+# if [ ! -d "/run/mysqld" ]; then
+# 	mkdir -p /run/mysqld
+# 	chown -R mysql:mysql /run/mysqld
+# fi
 
-service mysql start
+# if [ ! -d "/var/lib/mysql/mysql" ]; then
+# 	chown -R mysql:mysql /var/lib/mysql
 
-db_name = ${DB_NAME}
-db_user = ${DB_USER}
-db_pwd = ${DB_PWD}
+# 	# service mariadb start
 
-echo "CREATE DATABASE IF NOT EXISTS $db_name ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$db_user'@'%' IDENTIFIED BY '$db_pwd' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+# 	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql -rpm > dev/null
 
-mysql < db1.sql
 
-mysqld
+# 	echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
+# 	echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD' ;" >> db1.sql
+# 	echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';" >> db1.sql
+# 	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
+# 	echo "FLUSH PRIVILEGES;" >> db1.sql
+
+# 	# kill $(cat /var/run/mysqld/mysqld.pid)
+# 	mysqld --user=mysql --bootstrap < db1.sql
+# 	# mysql < db1.sql
+# fi
+
+# exec mysqld --user=mysql --console
+
+#!/bin/sh
+
+if [ ! -d "/run/mysqld" ]; then
+    mkdir -p /run/mysqld
+    chown -R mysql:mysql /run/mysqld
+fi
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+
+    chown -R mysql:mysql /var/lib/mysql
+
+    # init database
+    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+
+    tfile=mktemp
+    if [ ! -f "$tfile" ]; then
+        return 1
+    fi
+
+    # https://stackoverflow.com/questions/10299148/mysql-error-1045-28000-access-denied-for-user-billlocalhost-using-passw
+    cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+
+DELETE FROM    mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';
+
+CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$DB_USER'@'%' IDENTIFIED by '$DB_PWD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+
+FLUSH PRIVILEGES;
+EOF
+    # run init.sql
+    /usr/bin/mysqld --user=mysql --bootstrap < $tfile
+    rm -f $tfile
+fi
+
+exec /usr/bin/mysqld --user=mysql --console
